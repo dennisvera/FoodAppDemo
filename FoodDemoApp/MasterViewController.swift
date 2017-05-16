ee19994de1ea9b4d2a0d029364cb6774c5349a58
dennisvera <dennisvera.z@gmail.com>
2017-05-15 21:26:26 -0400

    pageMenu not working yet

--- FoodDemoApp/MasterViewController.swift
+++ FoodDemoApp/MasterViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MasterViewController: UICollectionViewController {
     
+    var pageMenu : CAPSPageMenu?
+    
     var foodImageArray = [UIImage]()
     
     override func viewDidLoad() {
@@ -23,6 +25,37 @@ class MasterViewController: UICollectionViewController {
         layout.minimumLineSpacing = 2
         layout.sectionInset = UIEdgeInsets(top: 2, left: 0, bottom: 2, right: 0)
         layout.maximumStretchHeight = collectionView!.bounds.width
+        
+        // Array to keep track of controllers in page menu
+        var controllerArray : [UIViewController] = []
+        
+        // Create variables for all view controllers you want to put in the
+        // page menu, initialize them, and add each to the controller array.
+        // (Can be any UIViewController subclass)
+        // Make sure the title property of all view controllers is set
+        // Example:
+        var controller : UIViewController = UIViewController(nibName: "controllerNibName", bundle: nil)
+        controller.title = "SAMPLE TITLE"
+        controllerArray.append(controller)
+        
+        // Customize page menu to your liking (optional) or use default settings by sending nil for 'options' in the init
+        // Example:
+        var parameters: [CAPSPageMenuOption] = [
+            .menuItemSeparatorWidth(4.3),
+            .useMenuLikeSegmentedControl(true),
+            .menuItemSeparatorPercentageHeight(0.1)
+        ]
+        
+        
+        // Initialize page menu with controller array, frame, and optional parameters
+//        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+        
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), CAPSPageMenuOption: parameters)
+        
+        // Lastly add page menu as subview of base view controller view
+        // or use pageMenu controller in you view hierachy as desired
+        self.view.addSubview(pageMenu!.view)
+        
         
         foodImageArray = [#imageLiteral(resourceName: "ceviche1"), #imageLiteral(resourceName: "ceviche2"), #imageLiteral(resourceName: "ceviche3"), #imageLiteral(resourceName: "ceviche1"), #imageLiteral(resourceName: "ceviche2"), #imageLiteral(resourceName: "ceviche3"), #imageLiteral(resourceName: "ceviche1"), #imageLiteral(resourceName: "ceviche2"), #imageLiteral(resourceName: "ceviche3"), #imageLiteral(resourceName: "ceviche1"), #imageLiteral(resourceName: "ceviche2"), #imageLiteral(resourceName: "ceviche3")]
     }
