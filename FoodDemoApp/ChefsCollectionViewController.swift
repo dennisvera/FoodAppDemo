b04aab42487f9286ade942e0b1d266910b0bcfd3
dennisvera <dennisvera.z@gmail.com>
2017-05-17 21:01:54 -0400

    chef controller shows all chefs from plist file

--- FoodDemoApp/ChefsCollectionViewController.swift
+++ FoodDemoApp/ChefsCollectionViewController.swift
@@ -12,13 +12,15 @@ class ChefsCollectionViewController: UICollectionViewController {
     
     let chefsData = Chefs.loadChefs()
     let columns: CGFloat = 2.0
-    let inset: CGFloat = 4.0
-    let spacing: CGFloat = 2.0
-    let lineSpacing: CGFloat = 4.0
+    let inset: CGFloat = 6.0
+    let spacing: CGFloat = 4.0
+    let lineSpacing: CGFloat = 6.0
     var isRandom = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationBar()
         
         // Refresh Control
         let refreshControl = UIRefreshControl()
@@ -30,6 +32,14 @@ class ChefsCollectionViewController: UICollectionViewController {
         isRandom = true
         collectionView?.reloadData()
         collectionView?.refreshControl?.endRefreshing()
+    }
+    
+    func navigationBar() {
+        navigationItem.title = "Chefs"
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.red]
+        let attrs = [NSForegroundColorAttributeName: UIColor.red, NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24)!]
+        
+        UINavigationBar.appearance().titleTextAttributes = attrs
     }
     
     // MARK: UICollectionViewDataSource
@@ -73,8 +83,6 @@ extension ChefsCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return lineSpacing
     }
-    
-    
     
 }
 
